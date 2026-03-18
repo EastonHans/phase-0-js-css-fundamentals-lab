b18fa9a7284ed8f457c63e68d87787af028fe6f5
Easton Hans <hanseaston697@gmail.com>
2026-03-18 06:42:00 +0300

    Update to lab css

---
+++ script.js
@@ -0,0 +1,80 @@
+// let student = "John Doe"
+// let studentOne = "Jane Doe"
+
+// let beatriceStudents = ["John Doe", "Jane Doe", "Jack Smith", "Emily Davis"]
+
+// console.log(beatriceStudents)
+
+// console.log (typeof beatriceStudents) 
+
+// console.log(beatriceStudent)
+
+// let price = 50
+// let discount = 10
+
+// let total = price + discount
+
+// // console.log(total)
+// console.log("price: ", price)
+// console.log("discount: ", discount)
+// console.log("total: ", total)
+
+// total = price - discount
+
+// console.log("total: ", total)
+
+// const scores = [85, 90, 78, 92, 88]
+
+
+
+// scores.splice(2, 2, 80)
+// console.log(scores)
+
+// for (let s = 0; s < scores.length; s++) {
+//     console.log(scores[s])
+// }
+
+// for (const s of scores) {
+//     console.log(s);
+// }
+
+
+
+// console.log(scores.length)
+
+// scores.push(95)
+// console.log(scores)
+
+// scores.pop()
+// console.log(scores)
+
+// scores.unshift(80)
+// console.log(scores)
+
+// scores.shift()
+// console.log(scores)
+
+// console.log(scores.indexOf(88))
+
+// console.log(scores[1])
+// console.log(scores.includes(50))
+// console.log(scores.includes(90))
+
+// console.log(scores[4])
+
+// console.log(scores[1])
+
+
+const technicalMentor = {
+    name: "Beatrice",
+    age: 30,
+    subject: "Javascript",
+    phoneNumber: "+254712345678",
+}
+
+// console.log(technicalMentor.name)
+
+technicalMentor.email = "beatrice.wambui@moringaschool.com"
+
+delete technicalMentor.age
+console.log(technicalMentor)
